6da3ec364584e0d4b2da7c7290983dab45cbf5f0
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-09-09 15:01:05 +0800

    Make it to all capital letters

--- src/pages/dashboard/setting/setting.jsx
+++ src/pages/dashboard/setting/setting.jsx
@@ -626,8 +626,8 @@ export function Setting() {
                   </label>
                   <Select
                     options={[
-                      { label: "Male", value: "male" },
-                      { label: "Female", value: "female" },
+                      { label: "Male", value: "Male" },
+                      { label: "Female", value: "Female" },
                     ]}
                     value={formik.values.gender}
                     onChange={(option) =>
